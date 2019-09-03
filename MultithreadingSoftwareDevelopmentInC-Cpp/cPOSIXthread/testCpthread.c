8b1031bddb222b711084483963ce5f0599d27675
jahic <jahic@lap598.iese.fhg.de>
2019-09-03 20:18:53 +0200

    POSIX thread example added.

--- MultithreadingSoftwareDevelopmentInC-Cpp/cPOSIXthread/testCpthread.c
+++ MultithreadingSoftwareDevelopmentInC-Cpp/cPOSIXthread/testCpthread.c
@@ -1,6 +1,6 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <unistd.h>  //Header file for sleep(). man 3 sleep for details. 
+#include <unistd.h>  
 #include <pthread.h> 
   
 // A normal C function that is executed as a thread  
